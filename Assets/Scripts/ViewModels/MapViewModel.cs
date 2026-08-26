21c6ba858b7d4de67f741f981ba3da2033778354
Katie Pustolski <pustolskik@gmail.com>
2026-08-26 15:53:03 -0700

    Update MapViewModel

--- Assets/Scripts/ViewModels/MapViewModel.cs
+++ Assets/Scripts/ViewModels/MapViewModel.cs
@@ -1,12 +1,25 @@
-using UnityEngine.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
 public class MapViewModel : IViewModel
 {
-    // (int: Rows, int: Columns)
-    public ObservableProperty<Map> Map {get; set;} = new();
+    public ObservableList<Tile> TileList {get; set;} = new();
+
+    public Map Map {get; set;}
+    public int TotalRows => (Map != null) ? Map.TotalRows : 0;
+    public int TotalColumns => (Map != null) ? Map.TotalColumns : 0;
 
     public MapViewModel(Map map)
     {
-        Map.SetAsMutable(map);
+        Map = map;
+        TileList.SetAsMutable(map._tileList);
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine($"Map: [{Map}]");
+        return sb.ToString();
     }
 }
