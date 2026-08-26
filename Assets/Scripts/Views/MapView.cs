b4108db32c0862a51bd7e51c8fb8e35b32b920c1
Katie Pustolski <pustolskik@gmail.com>
2026-08-26 15:58:36 -0700

    Updated MapView.cs
    * Added tile generation logic

--- Assets/Scripts/Views/MapView.cs
+++ Assets/Scripts/Views/MapView.cs
@@ -1,11 +1,24 @@
+using System;
+using System.Collections.Generic;
 using System.Data;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MapView : MonoBehaviour, IViewModelReceiver<MapViewModel>
 {
+    #region Unity References
+    [Header("Asset References")]
+    [SerializeField]
+    private Transform _tileViewParentTransform;
+
+    [Header("Prefabs")]
+    [SerializeField]
+    private TileView _tileViewPrefab;
+    #endregion
+
     private MapViewModel _mapViewModel;
-    private int _totalRows;
-    private int _totalColumns;
+    private List<TileView> _tileViewList = new List<TileView>();
+    private List<IDisposable> _viewModelSubscriptionList = new List<IDisposable>();
 
     public void SetViewModel(MapViewModel viewModel)
     {
@@ -18,8 +31,9 @@ public class MapView : MonoBehaviour, IViewModelReceiver<MapViewModel>
     {
         if (viewModel == null)
             return;
-
-        viewModel.Map.DidChange += OnMapChange;
+        
+        var tileListSub = viewModel.TileList.Subscribe(OnTileListChange);
+        _viewModelSubscriptionList.Add(tileListSub);
     }
 
     private void UnSubscribeToViewModel(MapViewModel viewModel)
@@ -27,14 +41,51 @@ public class MapView : MonoBehaviour, IViewModelReceiver<MapViewModel>
         if (viewModel == null)
             return;
         
-        viewModel.Map.DidChange -= OnMapChange;
+        // Make sure to remove the callback from the DidChange action.
+        foreach (var subscription in _viewModelSubscriptionList)
+        {
+            subscription.Dispose();
+        }
     }
 
-    private void OnMapChange(Map _, Map newValue)
+    private void OnTileListChange(List<Tile> _, List<Tile> newValue)
     {
-        _totalRows = newValue.TotalRows;
-        _totalColumns = newValue.TotalColumns;
+        // TODO: Test try catch loop
+        try
+        {
+            // If tile views don't exist yet, create a new series of them.
+            if (_tileViewList.Count == 0)
+            {
+                foreach (var tile in newValue)
+                {
+                    TileView newView = CreateTileView(tile);
+                    _tileViewList.Add(newView);
+                }
+            }
+            // Otherwise, update the existing map
+            else
+            {
+                //TODO: TBD. May not be necessary
+            }
+        }
+        catch (Exception e)
+        {
+            this.LogError($"Unable to create a map view. Error: {e}");
+        }
+    }
 
-        this.Log($"rows: {_totalRows}, columns: {_totalColumns}");
+    private TileView CreateTileView(Tile tile)
+    {
+        if (_tileViewPrefab == null)
+        {
+            throw new Exception("_tileViewPrefab is null!");
+        }
+
+        TileViewModel newViewModel = new TileViewModel(tile);
+        TileView newView = Instantiate(_tileViewPrefab, _tileViewParentTransform);
+        newView.gameObject.name = $"Tile_{newViewModel.Tile.Value.Point}";
+        newView.SetViewModel(newViewModel);
+
+        return newView;
     }
 }
