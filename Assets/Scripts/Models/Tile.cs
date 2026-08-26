e663bf3a70aa22a796d18cf2d5646e2c5d350e1b
Katie Pustolski <pustolskik@gmail.com>
2026-08-26 15:55:01 -0700

    Updated Tile.cs
    * Removed Id property. Wasn't needed.
    * Updated ToString()

--- Assets/Scripts/Models/Tile.cs
+++ Assets/Scripts/Models/Tile.cs
@@ -2,21 +2,15 @@ using System.Text;
 
 public struct Tile
 {
-    public int Id {get;}
     public Point Point {get;}
 
-    public Tile(int id, Point point)
+    public Tile(Point point)
     {
-        Id = id;
         Point = point;
     }
 
     public override string ToString()
     {
-        StringBuilder sb = new StringBuilder();
-        sb.AppendLine($"Id: {Id}");
-        sb.AppendLine($"Point: {Id}");
-
-        return sb.ToString();
+        return $"{Point}";
     }
 }
