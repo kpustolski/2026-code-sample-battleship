69f9388a8d93faa14252dd8b91057b977e98cbe9
Katie Pustolski <pustolskik@gmail.com>
2026-08-26 15:52:43 -0700

    Updated Map.cs
    * Added Tile list variables
    * Updated ToString()
    * Added CreateTileList()

--- Assets/Scripts/Models/Map.cs
+++ Assets/Scripts/Models/Map.cs
@@ -1,25 +1,52 @@
+using System;
+using System.Collections.Generic;
 using System.Text;
 
 //! https://www.youtube.com/watch?v=WwkuAqObplU Data oriented design
-public struct Map
+public class Map
 {
     public int Id {get;}
     public int TotalRows {get;}
     public int TotalColumns {get;}
+    public List<Tile> _tileList = new List<Tile>();
+    public List<Tile> TileList => _tileList;
 
     public Map (int id, int rows, int columns)
     {
         Id = id;
         TotalRows = rows;
         TotalColumns = columns;
+
+        CreateTileList();
     }
 
     public override string ToString()
     {
         StringBuilder sb = new StringBuilder();
-        sb.AppendLine($"TotalRows: {TotalRows}");
-        sb.AppendLine($"TotalColumns: {TotalColumns}");
-        
+        sb.Append($"Id: {Id}, ");
+        sb.Append($"TotalRows: {TotalRows}, ");
+        sb.Append($"TotalColumns: {TotalColumns}, ");
+        sb.Append($"TileList.Count: {TileList.Count}");
         return sb.ToString();
+    }
+
+    public void CreateTileList()
+    {
+        if (TotalRows != TotalColumns)
+        {
+            this.LogError("The number of rows must equal the number of columns!");
+            return;
+        }
+
+        for (int i = 0; i < TotalRows; i++)
+        {
+            for (int j = 0; j < TotalColumns; j++)
+            {
+                Point point = new Point(i,j);
+                Tile newTile = new Tile(point);
+
+                _tileList.Add(newTile);
+            }
+        }
     }
 }
