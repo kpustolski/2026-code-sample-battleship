385f28e884576ceb6f6f4f535892ded1b8e4db68
Katie Pustolski <pustolskik@gmail.com>
2026-08-26 15:50:31 -0700

    Updated Observable Property

--- Assets/Scripts/ObservableProperty.cs
+++ Assets/Scripts/ObservableProperty.cs
@@ -14,6 +14,7 @@ public class ObservableProperty<T>
         }
     }
 
+    public bool HasValue => Value != null;
     public event Action<T, T> DidChange;
 
     public void SetAsMutable(T newValue)
@@ -22,7 +23,22 @@ public class ObservableProperty<T>
         {
             T oldValue = _value;
             _value = newValue;
-            this.DidChange?.Invoke(oldValue, newValue);
+            DidChange?.Invoke(oldValue, newValue);
         }
+    }
+
+    public IDisposable Subscribe(Action<T, T> callback)
+    {
+        callback(default, Value);
+        DidChange += callback;
+        return new Unsubscriber(() => DidChange -= callback);
+    }
+
+    // Note from Calude
+    private sealed class Unsubscriber : IDisposable
+    {
+        private readonly Action _unsubscribe;
+        public Unsubscriber(Action unsubscribe) => _unsubscribe = unsubscribe;
+        public void Dispose() => _unsubscribe();
     }
 }
