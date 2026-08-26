3a7a0f0e8c88a40aa24a1a68db0f0036e0493afc
Katie Pustolski <pustolskik@gmail.com>
2026-08-26 15:51:26 -0700

    Updated TileViewModel.cs
    * Removed world position properties
    * Updated Point property

--- Assets/Scripts/ViewModels/TileViewModel.cs
+++ Assets/Scripts/ViewModels/TileViewModel.cs
@@ -3,15 +3,11 @@ using UnityEngine.Tilemaps;
 public class TileViewModel : IViewModel
 {
     public ObservableProperty<Tile> Tile {get; set;} = new();
-    public ObservableProperty<float> WorldPositionX {get; set;} = new();
-    public ObservableProperty<float> WorldPositionY {get; set;} = new();
 
-    public Point Point => (Tile?.Value == null) ? new Point(0,0) : Tile.Value.Point;
+    public Point Point => Tile.HasValue ? new Point(0,0) : Tile.Value.Point;
 
-    public TileViewModel(Tile tile, float worldPosX, float worldPosY)
+    public TileViewModel(Tile tile)
     {
         Tile.SetAsMutable(tile);
-        WorldPositionX.SetAsMutable(worldPosX);
-        WorldPositionY.SetAsMutable(worldPosY);
     }
 }
