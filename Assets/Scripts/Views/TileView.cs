368d73759d4287ecc12757959a97084474546dff
Katie Pustolski <pustolskik@gmail.com>
2026-08-26 15:51:58 -0700

    Updated TileView
    * Removed world position calls here too

--- Assets/Scripts/Views/TileView.cs
+++ Assets/Scripts/Views/TileView.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class TileView : MonoBehaviour, IViewModelReceiver<TileViewModel>
 {
     private TileViewModel _tileViewModel;
-    private Vector2 _worldPosition;
 
     public void SetViewModel(TileViewModel viewModel)
     {
@@ -18,47 +17,11 @@ public class TileView : MonoBehaviour, IViewModelReceiver<TileViewModel>
     {
         if (viewModel == null)
             return;
-        
-        viewModel.WorldPositionX.DidChange += OnWorldPositionXDidChange;
-        viewModel.WorldPositionY.DidChange += OnWorldPositionYDidChange;
     }
 
     private void UnSubscribeToViewModel(TileViewModel viewModel)
     {
         if (viewModel == null)
             return;
-        
-        viewModel.WorldPositionX.DidChange -= OnWorldPositionXDidChange;
-        viewModel.WorldPositionY.DidChange -= OnWorldPositionYDidChange;
-    }
-
-    private void OnWorldPositionXDidChange(float oldValue, float newValue)
-    {
-        if (_worldPosition == null)
-        {
-            _worldPosition = new Vector2(newValue, 0);
-        }
-        else
-        {
-            float currentYPos = _worldPosition.y;
-            _worldPosition = new Vector2(newValue, currentYPos);
-        }
-
-        this.Log($"oldValue: {oldValue} | newValue: {newValue}");
-    }
-    
-    private void OnWorldPositionYDidChange(float oldValue, float newValue)
-    {
-        if (_worldPosition == null)
-        {
-            _worldPosition = new Vector2(0, newValue);
-        }
-        else
-        {
-            float currentXPos = _worldPosition.x;
-            _worldPosition = new Vector2(currentXPos, newValue);
-        }
-
-        this.Log($"oldValue: {oldValue} | newValue: {newValue}");
     }
 }
