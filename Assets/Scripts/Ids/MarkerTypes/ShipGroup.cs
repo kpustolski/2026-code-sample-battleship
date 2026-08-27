ce7169e452b713c08036557008fc1d6f972fa8f9
Katie Pustolski <pustolskik@gmail.com>
2026-08-27 14:19:44 -0700

    Added ShipGroup id.

---
+++ Assets/Scripts/Ids/MarkerTypes/ShipGroup.cs
@@ -0,0 +1,4 @@
+public sealed class ShipGroup
+{
+    public ShipGroup(){}
+}
---
+++ Assets/Scripts/Ids/ShipGroupIds.cs
@@ -0,0 +1,5 @@
+public static class ShipGroupIds
+{
+    public static readonly Id<ShipGroup> None = new(nameof(None));
+    public static readonly Id<ShipGroup> Default = new(nameof(Default));
+}
